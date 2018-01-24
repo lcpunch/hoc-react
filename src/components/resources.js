e98e4bd0769ff6cda51f5471b0222c56f9aa28e0
lcpunch <lcpunchirolli@gmail.com>
2018-01-24 01:32:03 -0500

    initial commit

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+
+    return(
+        <div>
+            Super Secret for hangover
+            <ul>
+                <li>One egg</li>
+                <li>Pepper</li>
+                <li>Hair of the dog that bit ya</li>
+            </ul>
+        </div>
+    );
+
+};
